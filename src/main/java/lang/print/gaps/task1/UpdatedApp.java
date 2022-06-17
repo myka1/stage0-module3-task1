33ef5407bda0f9063d85aadc3a3131cf90c178f7
myka1 <msmykolas@gmail.com>
2022-06-17 21:36:18 +0300

    Update UpdatedApp.java

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("I'm updated.");
-        System.out.println("can be compiled. And can write to console");
+        System.out.println("updated");
     }
 }
